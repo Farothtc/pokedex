c78236adf8d9494bc454970b48c11f005b33660f
Farothtc <mvkirimi@gmail.com>
2024-10-10 17:54:20 +0300

    pokedata function added for axios.get

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,30 @@
-import { useState } from 'react'
+import { useState } from "react"
+import axios from "axios";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [dataAPI, setDataAPI] = useState({});
+  
+  const POKEMON_API = "https://pokeapi.co/api/v2/pokemon"
+
+async function pokedata(){
+    axios.get(POKEMON_API)
+    .then(response => {
+      setDataAPI(response.data)
+      console.log(response.data)
+    })
+    .catch(error => {
+      console.log("Error fetching data:",error)
+    })
+    
+    
+}
+
 
   return (
     <>
-      
+      <div>
+        <button onClick={pokedata}>Fetch Pokemon Data</button>
+      </div>
     </>
   )
 }
