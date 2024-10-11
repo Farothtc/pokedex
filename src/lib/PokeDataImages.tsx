1f370189ed356ea3114f03b72003b4b69d321efe
Farothtc <mvkirimi@gmail.com>
2024-10-11 19:08:40 +0300

    Initial commit

---
+++ src/lib/PokeDataImages.tsx
@@ -0,0 +1,26 @@
+export default [
+  {
+    id: 0,
+    img: "./src/assets/images/bulbasaur.png",
+  },
+  {
+    id: 1,
+    img: "./src/assets/images/bulbasaur.png",
+  },
+  {
+    id: 2,
+    img: "./src/assets/images/bulbasaur.png",
+  },
+  {
+    id: 3,
+    img: "./src/assets/images/bulbasaur.png",
+  },
+  {
+    id: 4,
+    img: "./src/assets/images/bulbasaur.png",
+  },
+  {
+    id: 5,
+    img: "./src/assets/images/bulbasaur.png",
+  },
+];
