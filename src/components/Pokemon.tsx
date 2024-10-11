10260283a69212185b17bcb8c46a82b763117d68
Farothtc <mvkirimi@gmail.com>
2024-10-11 19:09:26 +0300

    Pokeman component adjusted

--- src/components/Pokemon.tsx
+++ src/components/Pokemon.tsx
@@ -1,14 +1,27 @@
-interface PokemonProps {
+type PokemonDataProps = {
   e: {
     name: string;
     url: string;
   };
-}
+  img: {
+    id: number;
+    img: string;
+  };
+};
 
-export function Pokemon({ e }: PokemonProps) {
+export function Pokemon({ e, img }: PokemonDataProps) {
   return (
     <div className="col-3 my-5 poke--card">
-      <h2>{e.name}</h2>
+      <div className="card">
+        <img alt={"Picture of " + e.name} className="card-img-top" />
+        <div className="card-body">
+          <h5 className="card-title">{e.name}</h5>
+          <p className="card-text">
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
