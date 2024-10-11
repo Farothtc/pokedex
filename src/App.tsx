d447997d82a6c5e02187f2c562fbc11e45d81f85
Farothtc <mvkirimi@gmail.com>
2024-10-11 18:11:37 +0300

    async feat added for axios.get and ensured the call happen only once

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,34 @@
 import { Pokemon } from "./components/Pokemon";
 import axios from "axios";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
   const [dataAPI, setDataAPI] = useState([]);
 
   const POKEMON_API = "https://pokeapi.co/api/v2/pokemon?limit=100&offset=0";
 
-  function pokedata() {
-    axios
-      .get(POKEMON_API)
-      .then((response) => {
-        setDataAPI(response.data.results);
-        console.log(response.data.results);
-      })
-      .catch((error) => {
-        console.log("Error fetching data:", error);
-      });
+  async function pokedata() {
+    try {
+      const response = await axios.get(POKEMON_API);
+      setDataAPI(response.data.results);
+    } catch (error) {
+      console.log("Error fetching data:", error);
+    }
   }
+
+  useEffect(() => {
+    pokedata();
+  }, []);
 
   const PokeData = dataAPI.map((e, index) => <Pokemon key={index} e={e} />);
 
   return (
     <>
-      <div className="app--all">
-        {PokeData}
-        <button onClick={pokedata}>Fetch Pokemon Data</button>
+      <div className="container app--all">
+        <div className="row">
+          <h2 className="display-5 my-5 text-center">Pokedex</h2>
+          {PokeData}
+        </div>
       </div>
     </>
   );
