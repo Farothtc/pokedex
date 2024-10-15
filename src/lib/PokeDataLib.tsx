97df820a7638ddd41a84292565d2919b80fa4de2
Farothtc <mvkirimi@gmail.com>
2024-10-15 17:30:40 +0300

    Name fix and additional data added

---
+++ src/lib/PokeDataLib.tsx
@@ -0,0 +1,47 @@
+export default [
+  {
+    id: 0,
+    img: "./src/assets/images/bulbasaur.png",
+    pokeType: ["Grass", "Poison"],
+  },
+  {
+    id: 1,
+    img: "./src/assets/images/ivysaur.jpg",
+    pokeType: ["Grass", "Poison"],
+  },
+  {
+    id: 2,
+    img: "./src/assets/images/venusaur.png",
+    pokeType: ["Grass", "Poison"],
+  },
+  {
+    id: 3,
+    img: "./src/assets/images/charmender.png",
+    pokeType: "Fire",
+  },
+  {
+    id: 4,
+    img: "./src/assets/images/charmelon.png",
+    pokeType: "Fire",
+  },
+  {
+    id: 5,
+    img: "./src/assets/images/charizard.png",
+    pokeType: ["Fire", "Flying"],
+  },
+  {
+    id: 6,
+    img: "./src/assets/images/squirtle.png",
+    pokeType: "Water",
+  },
+  {
+    id: 7,
+    img: "./src/assets/images/wartotile.png",
+    pokeType: "Water",
+  },
+  {
+    id: 8,
+    img: "./src/assets/images/blastosite.png",
+    pokeType: "Water",
+  },
+];
