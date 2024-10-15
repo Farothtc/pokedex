697c0552bec7708488bc7073b237c1b3502008e4
Farothtc <mvkirimi@gmail.com>
2024-10-15 17:31:15 +0300

    Types and titles displayed UI

--- src/components/Pokemon.tsx
+++ src/components/Pokemon.tsx
@@ -6,16 +6,28 @@ type PokemonDataProps = {
   img: {
     id: number;
     img: string;
+    pokeType: string | string[];
   };
 };
 
 export function Pokemon({ e, img }: PokemonDataProps) {
   return (
-    <div className="col-3 my-5 poke--card">
+    <div className="col-4 my-5 poke--card">
       <div className="card">
-        <img alt={"Picture of " + e.name} className="card-img-top" />
+        <img src={img.img} className="card-img-top border-bottom" />
         <div className="card-body">
-          <h5 className="card-title">{e.name}</h5>
+          <div className="title--wrapper">
+            <h5 className="card-title">
+              {e.name.charAt(0).toUpperCase() + e.name.slice(1)}
+            </h5>
+            <h5 className="card-title">
+              {Array.isArray(img.pokeType)
+                ? img.pokeType.length > 1
+                  ? img.pokeType[0] + "  " + img.pokeType[1]
+                  : img.pokeType[0]
+                : img.pokeType}
+            </h5>
+          </div>
           <p className="card-text">
             Some quick example text to build on the card title and make up the
             bulk of the card's content.
