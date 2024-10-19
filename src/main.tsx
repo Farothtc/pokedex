674df2278763f7d2dacdee88ccbad93545b17830
Farothtc <mvkirimi@gmail.com>
2024-10-19 17:28:07 +0300

    React router added

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,28 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import { PokeStats } from "./components/PokeStats.tsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: (
+      <div>
+        <h2 className="display-5 text-center mt-5">Error 404</h2>
+      </div>
+    ),
+  },
+  {
+    path: "/pokestats/:pokemonName",
+    element: <PokeStats />,
+  },
+]);
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
