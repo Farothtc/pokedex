c8368bc2cb6b15275008b5ffdc0f64c46bba8cf9
Farothtc <mvkirimi@gmail.com>
2024-10-19 17:27:56 +0300

    React router, pokemon type and name added

--- src/components/Pokemon.tsx
+++ src/components/Pokemon.tsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 type PokemonDataProps = {
   e: {
     name: string;
@@ -11,27 +13,59 @@ type PokemonDataProps = {
 };
 
 export function Pokemon({ e, img }: PokemonDataProps) {
+  // const [bulbo, setBulbo] = useState([]);
+
+  // const BULBO_API = "https://pokeapi.co/api/v2/pokemon/bulbasaur";
+
+  // async function bulboData() {
+  //   try {
+  //     const response = await axios.get(BULBO_API);
+  //     setBulbo(response.data);
+  //     // console.log(bulbo);
+  //   } catch (error) {
+  //     console.log("Error fetching data:", error);
+  //   }
+  // }
+
+  // console.log(bulbo);
+
+  // useEffect(() => {
+  //   bulboData();
+  // }, []);
+
   return (
     <div className="col-4 my-5 poke--card">
       <div className="card">
-        <img src={img.img} className="card-img-top border-bottom" />
+        <Link to={`/pokestats/${e.name}`}>
+          <img src={img.img} className="card-img-top border-bottom" />
+        </Link>
         <div className="card-body">
           <div className="title--wrapper">
-            <h5 className="card-title">
-              {e.name.charAt(0).toUpperCase() + e.name.slice(1)}
-            </h5>
-            <h5 className="card-title">
-              {Array.isArray(img.pokeType)
-                ? img.pokeType.length > 1
-                  ? img.pokeType[0] + "  " + img.pokeType[1]
-                  : img.pokeType[0]
-                : img.pokeType}
+            <h4 className="card-title">
+              <Link to={`/pokestats/${e.name}`}>
+                {e.name.charAt(0).toUpperCase() + e.name.slice(1)}
+              </Link>
+            </h4>
+            <h5 className="card-title mt-2">
+              {Array.isArray(img.pokeType) ? (
+                img.pokeType.map((type, index) => (
+                  <span
+                    key={index}
+                    className={`poke-type-${type.toLowerCase()}`}
+                  >
+                    {type}
+                  </span>
+                ))
+              ) : (
+                <span className={`poke-type-${img.pokeType.toLowerCase()}`}>
+                  {img.pokeType}
+                </span>
+              )}
             </h5>
           </div>
-          <p className="card-text">
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </p>
+          <div className="card-text mt-5">
+            <p>Weight:</p>
+          </div>
         </div>
       </div>
     </div>
