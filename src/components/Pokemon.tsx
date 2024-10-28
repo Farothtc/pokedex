8c3aed2e6e62ff5409151c5a08f7f288f42024f5
Farothtc <mvkirimi@gmail.com>
2024-10-28 19:52:13 +0300

    Type rearrnged and comment deleted

--- src/components/Pokemon.tsx
+++ src/components/Pokemon.tsx
@@ -7,32 +7,13 @@ type PokemonDataProps = {
   };
   img: {
     id: number;
+    name: string;
     img: string;
     pokeType: string | string[];
   };
 };
 
 export function Pokemon({ e, img }: PokemonDataProps) {
-  // const [bulbo, setBulbo] = useState([]);
-
-  // const BULBO_API = "https://pokeapi.co/api/v2/pokemon/bulbasaur";
-
-  // async function bulboData() {
-  //   try {
-  //     const response = await axios.get(BULBO_API);
-  //     setBulbo(response.data);
-  //     // console.log(bulbo);
-  //   } catch (error) {
-  //     console.log("Error fetching data:", error);
-  //   }
-  // }
-
-  // console.log(bulbo);
-
-  // useEffect(() => {
-  //   bulboData();
-  // }, []);
-
   return (
     <div className="col-4 my-5 poke--card">
       <div className="card">
@@ -52,22 +33,6 @@ export function Pokemon({ e, img }: PokemonDataProps) {
                 {e.name.charAt(0).toUpperCase() + e.name.slice(1)}
               </Link>
             </h4>
-            {/* <h5 className="card-title mt-2 float-end">
-              {Array.isArray(img.pokeType) ? (
-                img.pokeType.map((type, index) => (
-                  <span
-                    key={index}
-                    className={`poke-type-${type.toLowerCase()}`}
-                  >
-                    {type}
-                  </span>
-                ))
-              ) : (
-                <span className={`poke-type-${img.pokeType.toLowerCase()}`}>
-                  {img.pokeType}
-                </span>
-              )}
-            </h5> */}
           </div>
         </div>
       </div>
